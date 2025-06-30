cadfd3d9c23b4910bc8bc6f71ecf30766bb35b95
Astral <info@astralsolution.tech>
2025-06-30 05:26:35 +0200

    update dependencies and routes

--- krf-site/src/App.js
+++ krf-site/src/App.js
@@ -25,8 +25,8 @@ export default function App() {
             <Route path="/academics" element={<Academics />} />
             <Route path="/donate" element={<Donate />} />
             <Route path="/programmes" element={<Programmes />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/gallery" element={<Gallery />} />
+             <Route path="/impact" element={<Impact />} />
+             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
